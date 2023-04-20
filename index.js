9e48b6b522ddec73a362b07536fed638fa0f9584
thards89 <thards89@gmail.com>
2023-04-20 22:01:30 +0200

    user data fetched

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
+/* data imports */
+import User from "./models/User.js"
+import { dataUser } from "./data/index.js"
 
 /* Configuration */
 dotenv.config();
@@ -35,5 +38,10 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    // User.insertMany(dataUser)
+
+    
 })
 .catch((error) => console.log(`${error} did not connect`));
