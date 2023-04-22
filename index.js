a7178a7381b19210345e6ac206dd9fe169e63ab8
thards89 <thards89@gmail.com>
2023-04-22 13:24:29 +0200

    products backend set up

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js"
 
 /* data imports */
 import User from "./models/User.js"
-import { dataUser } from "./data/index.js"
+import Product from "./models/Product.js"
+import ProductStat from "./models/ProductStat.js"
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js"
 
 /* Configuration */
 dotenv.config();
@@ -41,6 +43,8 @@ mongoose.connect(process.env.MONGO_URL, {
 
     /* ONLY ADD DATA ONE TIME */
     // User.insertMany(dataUser)
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
 
     
 })
--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,31 @@
-/*2h20*/ 
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 100,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 5,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    rating: Number,
+    supply: Number,
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
