7d34034be8b8d27b01b9a4f5dbd1bf5703e492a7
thards89 <thards89@gmail.com>
2023-04-22 15:31:51 +0200

    customers backend set up

--- controllers/client.js
+++ controllers/client.js
@@ -1,6 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
-// import User from "../models/User.js";
+import User from "../models/User.js";
 // import Transaction from "../models/Transaction.js";
 // import getCountryIso3 from "country-iso-2-to-3";
 
@@ -26,14 +26,14 @@ export const getProducts = async (req, res) => {
   }
 };
 
-// export const getCustomers = async (req, res) => {
-//   try {
-//     const customers = await User.find({ role: "user" }).select("-password");
-//     res.status(200).json(customers);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// };
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 // export const getTransactions = async (req, res) => {
 //   try {
--- routes/client.js
+++ routes/client.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
   getProducts,
-//   getCustomers,
+  getCustomers,
 //   getTransactions,
 //   getGeography,
 } from "../controllers/client.js";
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 
 router.get("/products", getProducts);
-// router.get("/customers", getCustomers);
+router.get("/customers", getCustomers);
 // router.get("/transactions", getTransactions);
 // router.get("/geography", getGeography);
 
