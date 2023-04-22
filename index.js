2ed69e9324cd8e8dd34c6e44b5850c45b922a899
thards89 <thards89@gmail.com>
2023-04-22 21:34:44 +0200

    overview data setup

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
 /* data imports */
-import User from "./models/User.js"
-import Product from "./models/Product.js"
-import ProductStat from "./models/ProductStat.js"
-import Transaction from "./models/Transaction.js"
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
 
 /* Configuration */
 dotenv.config();
@@ -47,6 +48,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
 
     
 })
